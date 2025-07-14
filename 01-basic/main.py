c84f8f3c27c4c899bac101cd3ac9d9d53bbed6b1
vishal9510 <132331589+vishal9510@users.noreply.github.com>
2025-07-14 18:20:21 +0530

    first commit

---
+++ 01-basic/main.py
@@ -0,0 +1,22 @@
+name="pratham chauhan"
+user__password="pratahm@9417" 
+
+
+print(name) 
+print(user__password)
+
+
+name='red and white' 
+user1='yash'
+user2='ronak'
+user3='pratham'
+user4='rohit'
+
+
+print(name)
+print(user1)
+print(user2)
+print(user3)
+print(user4)
+
+
